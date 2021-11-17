ab60f4d3a35e834ca233149c11d02443ba453bf9
MartaMariz <61186044+MartaMariz@users.noreply.github.com>
2021-11-17 01:28:21 +0000

    login e signup

--- myscript.js
+++ myscript.js
@@ -11,21 +11,52 @@ function apagarBoard() {
     document.getElementById("board").style.display = "none";
 }
 
+function apagarLogIn() {
+    document.getElementById("login").style.display = "none";
+}
+
+function apagarSignUp() {
+    document.getElementById("signup").style.display = "none";
+}
+
+
 
 document.getElementById("botao_instrucoes").onclick = function() {
     apagarBoard();
+    apagarLogIn();
+    apagarSignUp();
     apagarConfig();
     document.getElementById("instrucoes").style.display = "block";
 }
 
 document.getElementById("botao_config").onclick = function() {
     apagarBoard();
+    apagarLogIn();
+    apagarSignUp();
     apagarInstrucoes();
     document.getElementById("config").style.display = "block";
 }
 
+document.getElementById("botao_login").onclick = function() {
+    apagarBoard();
+    apagarInstrucoes();
+    apagarSignUp();
+    apagarConfig();
+    document.getElementById("login").style.display = "block";
+}
+
+document.getElementById("botao_signup").onclick = function() {
+    apagarBoard();
+    apagarInstrucoes();
+    apagarLogIn();
+    apagarConfig();
+    document.getElementById("signup").style.display = "block";
+}
+
 document.getElementById("novojogo").onclick = function() {
     apagarConfig();
+    apagarLogIn();
+    apagarSignUp();
     apagarInstrucoes();
     document.getElementById("board").style.display = "flex";
 }
